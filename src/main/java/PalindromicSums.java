a01bcebf1c9d8166b4d4404fe385d814a9b7f31a
PatGuan <patrick.guan91@gmail.com>
2016-03-17 22:23:15 -0400

    Fixed a small number in which the palindrom was iterating over.

--- src/main/java/PalindromicSums.java
+++ src/main/java/PalindromicSums.java
@@ -6,7 +6,8 @@ import java.util.Set;
 /**
      The palindromic number 595595 is interesting because it can be written as the sum of consecutive squares: 62+72+82+92+102+112+12262+72+82+92+102+112+122 .
      The palindromic number 696696 is also nice because it can be written as 102+122+142+162102+122+142+162, where the bases form an arithmetic progression with common difference 22.
-     There are exactly eleven palindromes below one-thousand that can be written as consecutive square sums, and the sum of these palindromes is 41644164. Note that 1=02+121=02+12 has not been included as this problem is concerned with the squares of positive integers. Also, there has to be at least two terms in the sum.
+     There are exactly eleven palindromes below one-thousand that can be written as consecutive square sums, and the sum of these palindromes is 41644164.
+     Note that 1=02+121=02+12 has not been included as this problem is concerned with the squares of positive integers. Also, there has to be at least two terms in the sum.
      Given NN and dd, find the sum of all the numbers less than NN that are both palindromic and can be written as the sum of squares whose bases form an arithmetic progression with common difference dd.
      Input Format
      The first line of input contains TT, the number of test cases.
@@ -43,7 +44,7 @@ import java.util.Set;
 
 public class PalindromicSums {
 
-    public static int init(int numericSumLimit, int baseDifference) {
+    public int init(int numericSumLimit, int baseDifference) {
 
         int totalSumOfPalindromicValues = 0;
         Set<Integer> solutionSet = getPalindromicSums(numericSumLimit, baseDifference, 1);
@@ -53,7 +54,7 @@ public class PalindromicSums {
         return totalSumOfPalindromicValues;
     }
 
-    private static HashSet<Integer> getPalindromicSums(int numericSumLimit, int baseDifference, int startingIteratingPosition) {
+    private HashSet<Integer> getPalindromicSums(int numericSumLimit, int baseDifference, int startingIteratingPosition) {
         int i;
         HashSet<Integer> palindromicSums = new HashSet<Integer>();
         for (i = startingIteratingPosition; i < Math.sqrt(numericSumLimit); i+=baseDifference) {
@@ -63,16 +64,17 @@ public class PalindromicSums {
     }
 
 
-    private static HashSet<Integer> findPalindromicSumsForInteger(int numericSumLimit, int baseDifference, int startingIteratingPosition) {
+    private HashSet<Integer> findPalindromicSumsForInteger(int numericSumLimit, int baseDifference, int startingIteratingPosition) {
         int i;
         HashSet<Integer> palindromicSums = new HashSet<Integer>();
         int currentTotal = 0;
         for (i = startingIteratingPosition; i < Math.sqrt(numericSumLimit); i+=baseDifference) {
             currentTotal = currentTotal + i*i;
+            System.out.println(i);
             if (currentTotal > numericSumLimit) {
                 return palindromicSums;
             }
-            else if (currentTotal > 100 && isNumberPalindrome(currentTotal)) {
+            else if (isNumberPalindrome(currentTotal)) {
                 palindromicSums.add(currentTotal);
             }
         }
@@ -80,23 +82,22 @@ public class PalindromicSums {
     }
 
 
-    public static boolean isNumberPalindrome(int number) {
+    public boolean isNumberPalindrome(int number) {
         String stringNumber = String.valueOf(number);
         int i;
-        for (i = 0; i < stringNumber.length()/3; i++) {
-            if (stringNumber.charAt(i) != stringNumber.charAt(stringNumber.length() - i - 1)){
-                return false;
-            }
+        if (stringNumber.length() == 2 && stringNumber.charAt(0) == stringNumber.charAt(1)) {
+            return true;
         }
-        return true;
+        else if (stringNumber.length() > 2) {
+            for (i = 0; i < Math.ceil(new Double(stringNumber.length()/2.0)); i++) {
+                if (stringNumber.charAt(i) != stringNumber.charAt(stringNumber.length() - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
-
-
-    public static void main(String[] args) {
-        System.out.println(init(597, 1));
-        System.out.println(findPalindromicSumsForInteger(700, 5, 1));
-    }
-
 }
 
 
