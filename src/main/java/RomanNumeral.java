565bf0d3d0fc274e791a1fcaf00e50664fe590e4
PatGuan <patrick.guan91@gmail.com>
2016-05-13 00:54:40 -0400

    Cleaned up the code. Got rid of if statements. Works up till 20.

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,30 +1,44 @@
 package src.main.java;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RomanNumeral {
 
+    private static HashMap<Integer, String> mapOfArabicNumeralsToRoman = new HashMap<Integer, String>();
+    private static ArrayList<Integer> listOfRomanNumerals = new ArrayList<Integer>();
+
+    public static void initializeHashMappings() {
+        mapOfArabicNumeralsToRoman.put(new Integer(10), "X");
+        mapOfArabicNumeralsToRoman.put(new Integer(9), "IX");
+        mapOfArabicNumeralsToRoman.put(new Integer(5), "V");
+        mapOfArabicNumeralsToRoman.put(new Integer(4), "IV");
+
+        listOfRomanNumerals.add(10);
+        listOfRomanNumerals.add(9);
+        listOfRomanNumerals.add(5);
+        listOfRomanNumerals.add(4);
+    };
+
     public static String convertNumberArabicToRoman(int number) {
+
+        initializeHashMappings();
+
         StringBuilder sb = new StringBuilder();
 
-        if (number == 4) {
-            sb.append("IV");
-            number -= 4;
+        for (int i = 0; i < number; i++) {
+            if (number >= listOfRomanNumerals.get(i)) {
+                sb.append(mapOfArabicNumeralsToRoman.get(listOfRomanNumerals.get(i)));
+                number -= listOfRomanNumerals.get(i);
+            }
         }
-        if (number == 9) {
-            sb.append("IX");
-            number -= 9;
-        }
-        if (number >= 10) {
-            sb.append("X");
-            number -= 10;
-        }
-        if (number >= 5) {
-            sb.append("V");
-            number -= 5;
-        }
+
         for (int i = 0; i < number; i++) {
             sb.append("I");
         }
+
         return new String(sb);
     }
+
 }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -57,4 +57,30 @@ public class RomanNumeralTest {
     public void should_return_XI_given_11() {
         assertEquals(RomanNumeral.convertNumberArabicToRoman(11), "XI");
     }
+
+    @Test
+    public void should_return_XIV_given_14() {
+        assertEquals(RomanNumeral.convertNumberArabicToRoman(14), "XIV");
+    }
+
+    @Test
+    public void should_return_XV_given_15() {
+        assertEquals(RomanNumeral.convertNumberArabicToRoman(15), "XV");
+    }
+
+    @Test
+    public void should_return_XVI_given_16() {
+        assertEquals(RomanNumeral.convertNumberArabicToRoman(16), "XVI");
+    }
+
+    @Test
+    public void should_return_XIX_given_19() {
+        assertEquals(RomanNumeral.convertNumberArabicToRoman(19), "XIX");
+    }
+
+
+    @Test
+    public void should_return_XX_given_20() {
+        assertEquals(RomanNumeral.convertNumberArabicToRoman(20), "XX");
+    }
 }
